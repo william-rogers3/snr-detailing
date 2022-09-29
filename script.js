70f382bb18a0dec2281ba55c4f02b4e4d811f467
Billy <wrrogers3@gmail.com>
2022-09-29 14:29:32 -0400

    Edited landing image and created second seciton.

---
+++ script.js
@@ -0,0 +1,5 @@
+const homeTitles = document.querySelector(".home-titles");
+homeTitles.classList.add("home-title-onload");
+
+const homeContainer = document.querySelector(".home-container");
+homeContainer.classList.add("fade-in");
