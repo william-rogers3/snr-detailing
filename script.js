23e68f44d277dbf05118394e5880c24ebdb6f453
Billy <wrrogers3@gmail.com>
2022-09-30 15:59:58 -0400

    fixed issue with github pages not performing transitions.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+setTimeout(function(){
+
 const homeTitles = document.querySelector(".home-titles");
 homeTitles.classList.add("home-title-onload");
 
 const homeContainer = document.querySelector(".home-container");
 homeContainer.classList.add("fade-in");
+
+},1);
