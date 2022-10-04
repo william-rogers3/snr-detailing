71f023e08cb0bf2a0cf3b18e4484a23ef2faaeae
Billy <wrrogers3@gmail.com>
2022-10-04 14:15:55 -0400

    Added animations to about section.

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-setTimeout(function(){
+setTimeout(function () {
 
-const homeTitles = document.querySelector(".home-titles");
-homeTitles.classList.add("home-title-onload");
+    const homeTitles = document.querySelector(".home-titles");
+    homeTitles.classList.add("home-title-onload");
 
-const homeContainer = document.querySelector(".home-container");
-homeContainer.classList.add("fade-in");
+    const homeContainer = document.querySelector(".home-container");
+    homeContainer.classList.add("fade-in");
 
-},1);
+}, 1);
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
