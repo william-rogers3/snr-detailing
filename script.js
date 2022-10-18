845e9b4330e6f90d7905580723c9b6c81949c141
billy <wrrogers3@gmail.com>
2022-10-18 14:34:49 -0400

    Edited mobile layout. Resized elements to be more responsive.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var detailCost = new Map();
-detailCost.set('small-car', {0: '$59.99', 1: '$69.99', 2: '$89.99'});
-detailCost.set('sedan',{0: '$64.99', 1: '$74.99', 2: '$94.99'});
-detailCost.set('suv',{0: '$69.99', 1: '$89.99', 2: '$99.99'});
-detailCost.set('van',{0: '$99.99', 1: '$109.99', 2: '$119.99'});
-detailCost.set('truck',{0: '$99.99', 1: '$99.99', 2: '$129.99'});
+detailCost.set('small-car', {0: '$59.99', 1: '$69.99', 2: '$119.99', 3: '$89.99'});
+detailCost.set('sedan',{0: '$64.99', 1: '$74.99', 2: '$129.99', 3: '$94.99'});
+detailCost.set('suv',{0: '$69.99', 1: '$89.99', 2: '$149.99', 3: '$99.99'});
+detailCost.set('van',{0: '$99.99', 1: '$109.99', 2: '$199.99', 3: '$119.99'});
+detailCost.set('truck',{0: '$99.99', 1: '$99.99', 2: '$189.99', 3: '$129.99'});
 // values are for basic exterior, basic interior, premium exterior in that order.
 // values are labeled as numbers to simplify for loop.
 
@@ -11,7 +11,7 @@ function displayPricing(carType) {
     let carSelection = document.getElementById(`${carType}`);
     let prices = document.getElementsByClassName("price");
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < 4; i++) {
         prices[i].textContent = `${detailCost.get(`${carType}`)[`${i}`]}+`
     }
 }
