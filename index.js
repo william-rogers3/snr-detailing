5b916f6bfbe8cb8140922f06180910c2abd3d07a
Billy <wrrogers3@gmail.com>
2023-02-16 17:14:55 -0500

    Styled home section. Now shares same image as desktop web page.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello");
+setTimeout(function () {
+    const homeTitles = document.querySelector(".home-titles");
+    homeTitles.classList.add("home-title-onload");
+
+    const homeContainer = document.querySelector("#home-container");
+    homeContainer.classList.add("fade-in");
+}, 1);
