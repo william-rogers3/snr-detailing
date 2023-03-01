b229d928138fc42524676043d79eef8dd6f2b002
Billy <wrrogers3@gmail.com>
2023-03-01 13:32:12 -0500

    Created animations for hamburger menu.

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+function toggleMenu() {
+    const contentContainer = document.querySelector(".content-container");
+    const bars = document.getElementsByClassName("bar");
+    const body = document.body;
+
+    body.classList.toggle("overflow-blocked");
+    contentContainer.classList.toggle("active");
+
+    for (let i = 0; i < bars.length; i++) {
+        bars[i].classList.toggle("active");
+    }
+
+}
+
 setTimeout(function () {
     const homeTitles = document.querySelector(".home-titles");
     homeTitles.classList.add("home-title-onload");
