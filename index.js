4bd606d99c8c650e1356c1b6c85fe1f562b2aac8
billy <wrrogers3@gmail.com>
2023-03-07 23:04:03 -0500

    Created fading animation for links when hamburger menu is active. Links will now display when hamburger menu is active.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function toggleMenu() {
     const contentContainer = document.querySelector(".content-container");
+    const links = document.querySelector(".links");
     const bars = document.getElementsByClassName("bar");
     const body = document.body;
 
     body.classList.toggle("overflow-blocked");
     contentContainer.classList.toggle("active");
+    links.classList.toggle("active");
 
     for (let i = 0; i < bars.length; i++) {
         bars[i].classList.toggle("active");
@@ -20,7 +22,21 @@ setTimeout(function () {
     homeContainer.classList.add("fade-in");
 }, 1);
 
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } 
+        // else {
+        //     entry.target.classList.remove("show");
+        // }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
 
 
-let vh = window.innerHeight * 1;
+
+let vh = window.innerHeight;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
