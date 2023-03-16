0d8c44ccdc830e53f680bb18189cfcdbf4a9e58f
Billy <wrrogers3@gmail.com>
2023-03-16 17:10:45 -0400

    Edited font-sizing of most elements. Made pricing change depending on selected vehicle.

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
+var detailCost = new Map();
+detailCost.set('small-car', {
+    0: '$49.99',
+    1: '$79.99',
+    2: '$59.99'
+});
+detailCost.set('sedan', {
+    0: '$49.99',
+    1: '$79.99',
+    2: '$64.99'
+});
+detailCost.set('suv', {
+    0: '$64.99',
+    1: '$89.99',
+    2: '$69.99'
+});
+detailCost.set('van', {
+    0: '$89.99',
+    1: '$119.99',
+    2: '$94.99'
+});
+detailCost.set('truck', {
+    0: '$94.99',
+    1: '$119.99',
+    2: '$89.99'
+});
+// values are for basic exterior, premium exterior, basic interior in that order.
+// values are labeled as numbers to simplify for loop.
+
+function displayPricing(carType) {
+    let carSelection = document.getElementById(`${carType}`);
+    let prices = document.getElementsByClassName("price");
+
+    for (let i = 0; i < 3; i++) {
+        prices[i].textContent = `${detailCost.get(`${carType}`)[`${i}`]}+`
+    }
+}
+
+
 function toggleMenu() {
     if (window.innerWidth > 768) {
         return;
@@ -43,3 +82,18 @@ hiddenElements.forEach((el) => observer.observe(el));
 
 let vh = window.innerHeight;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+const carElements = document.getElementsByClassName("car");
+carElements[0].classList.add("selected");
+
+for (let i = 0; i < carElements.length; i++) {
+    carElements[i].addEventListener("click", () => {
+        for (let i = 0; i < carElements.length; i++) {
+            carElements[i].classList.remove("selected");
+        }
+        carElements[i].classList.add("selected");
+        const car = carElements[i].id;
+        displayPricing(car);
+    });
+
+}
